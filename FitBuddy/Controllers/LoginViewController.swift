3b370ce4fe00b40062fecb190365224124909cee
Wilson Ding <hello@wilsonding.com>
2017-11-04 23:24:26 -0500

    Added QR code for Login

--- FitBuddy/Controllers/LoginViewController.swift
+++ FitBuddy/Controllers/LoginViewController.swift
@@ -7,17 +7,84 @@
 //
 
 import UIKit
+import AVFoundation
 
 class LoginViewController: UIViewController {
+
+    @IBOutlet weak var previewView: UIView!
+
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView: UIView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let captureDevice = AVCaptureDevice.default(for: .video)
+
+        do {
+            captureSession = AVCaptureSession()
+
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession?.addInput(input)
+
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [.qr]
+
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.videoGravity = .resizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            previewView.layer.addSublayer(videoPreviewLayer!)
+
+            captureSession?.startRunning()
+
+            qrCodeFrameView = UIView()
+
+            if let qrCodeFrameView = qrCodeFrameView {
+                qrCodeFrameView.layer.borderColor = UIColor.orange.cgColor
+                qrCodeFrameView.layer.borderWidth = 2
+                view.addSubview(qrCodeFrameView)
+                view.bringSubview(toFront: qrCodeFrameView)
+            }
+        } catch {
+            print(error)
+            return
+        }
     }
-    
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        captureSession?.stopRunning()
+    }
+
     @IBAction func didPressBackButton(_ sender: Any) {
         performSegue(withIdentifier: "unwindToVC", sender: self)
     }
+}
 
+extension LoginViewController : AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+
+        if metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            return
+        }
+
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+
+            if metadataObj.stringValue != nil {
+                print(metadataObj.stringValue!)
+            }
+        }
+    }
 }
